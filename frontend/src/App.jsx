6954f63382cb0d2a3e6d997919e3ecca6be5c417
Prudhvikoganti <prudhvisk@eficens.com>
2025-04-14 08:37:57 +0530

    added login buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import LandingPage from "./modules/LandingPage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Header />
-      <main style={{ minHeight: '89vh' }}></main>
+      <main>
+        <LandingPage />
+      </main>
       <Footer />
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/modules/LandingPage.tsx
+++ frontend/src/modules/LandingPage.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import './LandingPage.css'
+import React from "react";
+import "./LandingPage.css";
+import { Button, Container, Row } from "react-bootstrap";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="into-text">
+            <div>
+              <h1 className="title">Welcome to Note Zipper</h1>
+              <p className="subtitle">One safe place for all your notes</p>
+            </div>
+            <div className="buttoncontainer">
+              <a href="/login">
+                <Button
+                  size="lg"
+                  className="landingbutton"
+                  variant="outline-primary"
+                >
+                  Login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button
+                  size="lg"
+                  className="landingbutton"
+                  variant="outline-primary"
+                >
+                  Register
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
